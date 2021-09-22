11a5f11cf0d20a739b3900c900354b65b297bd9e
Joey Sullivan <monkeydevtools@gmail.com>
2021-09-22 00:39:07 -0400

    Added set_ traverser_functions
    Signed-off-by: Joey Sullivan <monkeydevtools@gmail.com>

---
+++ src/treepath/path/util/match_to_path.py
@@ -0,0 +1,15 @@
+from treepath import Match
+from treepath.path.builder.path_builder import PathBuilder
+from treepath.path.builder.root_path_builder import RootPathBuilder
+
+
+def match_to_path(match: Match) -> PathBuilder:
+    """
+    Converts a match to a Path
+    """
+    path_as_list = match.path_as_list
+    path = RootPathBuilder()
+    for match in path_as_list[1:]:
+        data_name = match.data_name
+        path = path[data_name]
+    return path
